2950acac4c573fa8b7c0c510396a14ee9f352539
Dami Bayer <fotoshoot.flat@gmail.com>
2022-05-09 13:44:38 +0000

    Created Contacts table

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
-class Contact < ActiveRecord::Base
 
+class Contact < ActiveRecord::Base
+ validates :name, presence:true
+ validates :emails, presence:true
+ validates :comments, presence:true
 end
--- db/migrate/20220507183703_create_contacts.rb
+++ db/migrate/20220507183703_create_contacts.rb
@@ -4,6 +4,7 @@ class CreateContacts < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :email
       t.text :comments
+      
       t.timestamps
     end
   end
